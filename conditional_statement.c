34221a5db330a18d9dbe3f769076df49bb6ab87c
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-03-29 23:35:49 +0530

    Update conditional_statement.c

--- conditional_statement.c
+++ conditional_statement.c
@@ -1,1 +1,26 @@
 
+# include <stdio.h>
+
+int main() {
+  int marks;
+  scanf("%d",&marks);
+
+  if (marks>=85 && marks<=100) {
+    printf("You are eligible to take Cyber Security");
+  }
+  else if (marks>=75 && marks<85) {
+    printf("You are eligible to take Data Science but you are not eligible to take Cyber Security");
+  }
+  else if (marks>=40 && marks<75) {
+    printf("You are eligible for specilization except Data Science and Cyber Security");
+  }
+  else if (marks>=0 && marks<40) {
+    printf("You are not eligibke for any of the specialization, First clear your Backlog");
+  }
+  else {
+    printf("Invalid input")
+      }
+
+  return 0;
+
+}
