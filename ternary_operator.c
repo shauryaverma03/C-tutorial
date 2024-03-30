2069383cb34a774c6fff4e2e688016b0a3b4b926
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-03-31 02:21:24 +0530

    Update ternary_operator.c

--- ternary_operator.c
+++ ternary_operator.c
@@ -1,1 +1,9 @@
+# include <stdio.h>
 
+int main() {
+  int age;
+  printf("Enter Your age: ");
+  scanf("%d",&age);
+  age >= 18 ? printf("You are Eligible to Vote") : printf("You are Not Eligible to Vote");
+  return 0;
+}
