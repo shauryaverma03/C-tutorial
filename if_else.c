b7a9077353f0d0fdf9eb0f86dd445a4b4d1aa996
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-04-03 01:47:44 +0530

    Update if_else.c

--- if_else.c
+++ if_else.c
@@ -6,25 +6,25 @@ int main() {
   printf("Enter your marks: ");
   scanf("%d",&marks);
 
-  if (n>=90 && n<=100) {
+  if (marks>=90 && marks<=100) {
     printf("Grade: O");
   }
-  else if (n>=80 && n<=89) {
+  else if (marks>=80 && marks<=89) {
     printf("Grade: A");
   }
-  else if (n>=70 && n<=79) {
+  else if (marks>=70 && marks<=79) {
     printf("Grade: B");
   }
-  else if (n>=60 && n<=69) {
+  else if (marks>=60 && marks<=69) {
     printf("Grade: C");
   }
-  else if (n>=50 && n<=59) {
+  else if (marks>=50 && marks<=59) {
     printf("Grade: D");
   }
-  else if (n>=40 && n<=49) {
+  else if (marks>=40 && marks<=49) {
     printf("Grade: E");
   }
-  else if (n>=0 && n<40) {
+  else if (marks>=0 && marks<40) {
     printf("You are Fail");
   }
   else {
