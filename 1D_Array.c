aa3711d66a32fc10b5659fe1f5b1c004ef971f29
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-04-14 12:05:05 +0530

    Update 1D_Array.c

--- 1D_Array.c
+++ 1D_Array.c
@@ -1,1 +1,17 @@
+# include <stdio.h>
 
+int main() {
+  int n;
+  scanf("%d",&n);
+
+  int arr[n];
+  for (int i=0; i<n; i++) {
+    scanf("%d",&arr[i]);
+  }
+
+  for (int i=0; i<n; i++) {
+    printf("%d",arr[i]);
+  }
+
+  return 0;
+}
