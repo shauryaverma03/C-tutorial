7d03d63184382aa5718b6a48e85c82f8b282cf24
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-04-14 12:09:11 +0530

    Update 1D_Array.c

--- 1D_Array.c
+++ 1D_Array.c
@@ -2,8 +2,10 @@
 
 int main() {
   int n;
+  printf("Enter the length of an array: ");
   scanf("%d",&n);
 
+  printf("Enter the %d numbers in the array: ",n);
   int arr[n];
   for (int i=0; i<n; i++) {
     scanf("%d",&arr[i]);
