3b4817542fc1b049913b6b19cab6dca628240aae
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-04-16 19:23:19 +0530

    Update 2D_Array.c

--- 2D_Array.c
+++ 2D_Array.c
@@ -1,1 +1,23 @@
+# include <stdio.h>
 
+int main() {
+  int n;
+  printf("Enter the number of row & coloumn matrix you want to form: );
+  scanf("%d",&n);
+
+  int arr[n][n];
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      scanf("%d",&arr[i][j]);
+    }
+  }
+
+  for (int i=0; i<n; i++) {
+    for (int j=0; j<n; j++) {
+      printf("%d ",arr[i][i]);
+    }
+    printf("\n");
+  }
+  
+  return 0;
+}
