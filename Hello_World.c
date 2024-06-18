9c52d726da46657b48116e61dc0da5a41ef9397f
Shaurya Verma <143018626+shauryaverma03@users.noreply.github.com>
2024-06-18 23:53:33 +0530

    Update Hello_World.c

--- Hello_World.c
+++ Hello_World.c
@@ -5,6 +5,7 @@ int main(){
   printf("Welcome to my repository");
   printf("I am Learning C Programming");
   printf("Hello Github");
+  printf("I am a CSE student");
   printf("Bye");
   return 0;
 }
